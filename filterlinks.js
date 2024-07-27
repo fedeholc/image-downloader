a7682bf508b7e4ea58315f59bcb1b5c9ea78de0f
Federico Holc <federicoholc@gmail.com>
2024-07-26 21:07:49 -0300

    chore: Update image download logic and filter links

---
+++ filterlinks.js
@@ -0,0 +1,38 @@
+import fs from "fs";
+
+const directory = "./images-cappa";
+const linksFile = "./images-cappa/imgUrls.txt";
+
+//create an array with all the files in directory
+const files = fs.readdirSync(directory);
+console.log(files);
+
+//read linksFile line by line and check if the line string contains any of the strings in files
+
+const links = fs.readFileSync(linksFile, "utf-8").split("\n");
+console.log(links);
+
+let filteredLinks = new Set();
+for (let link of links) {
+  for (let file of files) {
+    //check if last part of link (a filename) is in the file string
+    if (link.includes(file)) {
+      console.log(link, file);
+      filteredLinks.add(link);
+    }
+  }
+}
+
+console.log(filteredLinks);
+
+//write filteredLinks to a json file
+fs.writeFileSync(
+  directory + "/filteredLinks.json",
+  JSON.stringify([...filteredLinks], null, 2)
+);
+
+//write filteredLinks to a new  text file
+fs.writeFileSync(
+  directory + "/filteredLinks.txt",
+  [...filteredLinks].join("\n")
+);
--- index.js
+++ index.js
@@ -4,17 +4,7 @@ import path from "path";
 import sharp from "sharp";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Función para descargar una imagen
-const downloadImage = async (url, outputPath) => {
-  const response = await fetch(url);
-  const buffer = await response.buffer();
-  await fs.promises.writeFile(outputPath, buffer);
-};
-
-async function getSubPages(pageUrl, pageMustInclude) {
+async function getSubPages(pageUrl, subPageMustInclude) {
   try {
     const response = await fetch(pageUrl);
     const html = await response.text();
@@ -23,23 +13,70 @@ async function getSubPages(pageUrl, pageMustInclude) {
     const $ = load(html);
 
     // Buscar todas las etiquetas de enlace
-    const linkUrls = $("a")
+    const subPages = $("a")
       .map((i, link) => $(link).attr("href"))
       .get();
 
-    console.log("Subpages:", linkUrls);
-
-    const rta = new Set(
-      linkUrls.filter((link) => link.includes(pageMustInclude))
+    const filteredSubPages = new Set(
+      subPages.filter((link) => link.includes(subPageMustInclude))
     );
-    return rta;
+
+    return filteredSubPages;
   } catch (err) {
     console.error(`Error al procesar la página ${pageUrl}: ${err.message}`);
   }
 }
 
-// Función para extraer y descargar imágenes de una página
-async function downloadImagesFromPage(pageUrl, imgMustInclude) {
+async function downloadImages(imagesUrls, imgMustInclude) {
+  try {
+    if (!fs.existsSync(imgOutputDir)) {
+      fs.mkdirSync(imgOutputDir);
+    }
+
+    console.log("t1", typeof imagesUrls);
+
+    for (let imgUrl of imagesUrls) {
+      // VER Sería para resolver URLs relativas (no lo probé aún)
+
+      console.log("t2", typeof imgUrl, imgUrl);
+
+      if (imgUrl.startsWith("//")) {
+        imgUrl = "http:" + imgUrl;
+      } else if (imgUrl.startsWith("/")) {
+        const url = new URL(imagesUrls);
+        imgUrl = url.origin + imgUrl;
+      }
+
+      if (!imgUrl.includes(imgMustInclude)) {
+        continue;
+      }
+
+      try {
+        const response = await fetch(imgUrl);
+        const arrayBuffer = await response.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        // Obtener las dimensiones de la imagen
+        const metadata = await sharp(buffer).metadata();
+
+        // Filtrar imágenes por tamaño
+        if (metadata.width > 100 || metadata.height > 100) {
+          await fs.promises.writeFile(
+            path.join(imgOutputDir, path.basename(imgUrl)),
+            buffer
+          );
+          fs.appendFileSync(imgUrlsFile, `${imgUrl}\n`);
+        }
+      } catch (err) {
+        console.error(`Error al descargar ${imgUrl}: ${err.message}`);
+      }
+    }
+  } catch (err) {
+    console.error(`Error al procesar la página ${imagesUrls}: ${err.message}`);
+  }
+}
+
+async function getImagesUrls(pageUrl) {
   try {
     // Evitar visitar la misma URL más de una vez
     if (visitedUrls.has(pageUrl)) {
@@ -58,69 +95,57 @@ async function downloadImagesFromPage(pageUrl, imgMustInclude) {
       .map((i, img) => $(img).attr("src"))
       .get();
 
-    // Crear un directorio para guardar las imágenes
-    const outputDir = path.join(__dirname, "images");
-    if (!fs.existsSync(outputDir)) {
-      fs.mkdirSync(outputDir);
-    }
+    const uniqueImgUrls = new Set(imgUrls);
 
-    for (let imgUrl of imgUrls) {
-      // Resolver URLs relativas
-      if (imgUrl.startsWith("//")) {
-        imgUrl = "http:" + imgUrl;
-      } else if (imgUrl.startsWith("/")) {
-        const url = new URL(pageUrl);
-        imgUrl = url.origin + imgUrl;
-      }
-
-      if (!imgUrl.includes(imgMustInclude)) {
-        continue;
-      }
-      try {
-        const response = await fetch(imgUrl);
-        const arrayBuffer = await response.arrayBuffer();
-        const buffer = Buffer.from(arrayBuffer);
-
-        // Obtener las dimensiones de la imagen
-        const metadata = await sharp(buffer).metadata();
-
-        // Filtrar imágenes por tamaño
-        if (metadata.width > 100 || metadata.height > 100) {
-          const imgName = path.basename(imgUrl);
-          const outputPath = path.join(outputDir, imgName);
-          await fs.promises.writeFile(outputPath, buffer);
-          console.log(`Descargada: ${imgUrl}`);
-        }
-      } catch (err) {
-        console.error(`Error al descargar ${imgUrl}: ${err.message}`);
-      }
-    }
+    return uniqueImgUrls;
   } catch (err) {
     console.error(`Error al procesar la página ${pageUrl}: ${err.message}`);
   }
 }
 
-async function getImagesFromPages(pages, imgMustInclude) {
-  console.log(pages);
-  for (let linkUrl of pages) {
-    // Resolver URLs relativas
-    if (linkUrl && !linkUrl.startsWith("http")) {
+async function getImages(pages, imgMustInclude) {
+  const imagesList = new Set();
+  for (let page of pages) {
+    //VER Sería para resolver URLs relativas, pero no lo probé aún
+    if (page && !page.startsWith("http")) {
       const url = new URL(pageUrl);
-      linkUrl = url.origin + linkUrl;
+      page = url.origin + page;
     }
-    console.log(linkUrl);
+
     // Evitar URLs vacías y anclas
-    if (linkUrl && !linkUrl.startsWith("#")) {
-      console.log(`Procesando: ${linkUrl}`);
-      await downloadImagesFromPage(linkUrl, imgMustInclude);
+    if (page && !page.startsWith("#")) {
+      console.log(`Procesando: ${page}`);
+
+      let urls = await getImagesUrls(page);
+      imagesList.add(...urls);
     }
   }
+
+  await downloadImages(Array.from(imagesList), imgMustInclude);
 }
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const visitedUrls = new Set();
-// URL de la página web
-const pageUrl = "https://www.magnumphotos.com/photographer/robert-capa/"; // Reemplaza con la URL deseada
 
-const subpages = await getSubPages(pageUrl, "par");
+const pageName = "robert-capa";
+const pageUrl = "https://www.magnumphotos.com/photographer/robert-capa/";
 
-await getImagesFromPages(subpages, "overlay");
+const subPageMustInclude = "par";
+const imgMustInclude = "overlay";
+
+const imgOutputDir = path.join(__dirname, "images-" + pageName);
+const imgUrlsFile = path.join(imgOutputDir, "imgUrls.txt");
+
+if (!fs.existsSync(imgOutputDir)) {
+  fs.mkdirSync(imgOutputDir);
+}
+fs.appendFileSync(imgUrlsFile, `Downloaded from ${pageUrl}\n`);
+
+const subpages = await getSubPages(pageUrl, subPageMustInclude);
+subpages.add(pageUrl);
+
+//console.log("Subpages:", subpages.size, subpages);
+
+await getImages(subpages, imgMustInclude);
