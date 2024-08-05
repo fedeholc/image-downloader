c9f30ebe3e1d3cd012a574788b60ce8f3406ea1e
Federico Holc <federicoholc@gmail.com>
2024-08-05 12:37:07 -0300

    chore: Refactor database initialization and table creation

--- init-db.ts
+++ init-db.ts
@@ -2,13 +2,69 @@ import fs from "fs";
 import sqlite3 from "sqlite3";
 const filepath = "./images.db";
 
+//const tables = { images: "images", authors: "authors", albums: "albums" };
+
+enum tableNames {
+  images = "images",
+  authors = "authors",
+  albums = "albums",
+}
+
+enum authorFields {
+  id = "id",
+  name = "name",
+  description = "description",
+  image = "image",
+}
+
+//delete the database file if it exists
+if (fs.existsSync(filepath)) {
+  fs.unlink(filepath, (error) => {
+    if (error) {
+      return console.error(error.message);
+    }
+  });
+}
+
+
+function insertRow() {
+  //const [name, color, weight] = process.argv.slice(2);
+  const [name, color, weight] = ["Jaws", "white", "2000"];
+  db.run(
+    `INSERT INTO ${tableNames.authors} (name, color, weight) VALUES (?, ?, ?)`,
+    [name, color, weight],
+    function (error) {
+      if (error) {
+        console.error(error.message);
+      }
+      console.log(`Inserted a row with the ID: ${this.lastID}`);
+    }
+  );
+}
 
 //create a database if it doesn't exist
 const db = createDbConnection();
 
-//if not exist create a table
-db.exec(
-  `CREATE TABLE IF NOT EXISTS images (
+deleteTables(db);
+createTables(db);
+
+db.close((error) => {
+  if (error) {
+    return console.error(error.message);
+  }
+});
+
+function deleteTables(db: sqlite3.Database) {
+  db.exec("DROP TABLE IF EXISTS images");
+  db.exec("DROP TABLE IF EXISTS authors");
+  db.exec("DROP TABLE IF EXISTS albums");
+
+}
+
+function createTables(db: sqlite3.Database) {
+
+  db.exec(
+    `CREATE TABLE IF NOT EXISTS images (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     url TEXT NOT NULL,
     description TEXT,
@@ -18,40 +74,41 @@ db.exec(
     FOREIGN KEY(authorId) REFERENCES authors(id),
     FOREIGN KEY(albumId) REFERENCES albums(id)
   )`,
-  (error) => {
-    if (error) {
-      return console.error(error.message);
+    (error) => {
+      if (error) {
+        return console.error(error.message);
+      }
     }
-  }
-);
+  );
 
-db.exec(
-  `CREATE TABLE IF NOT EXISTS authors (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    description TEXT,
-    image TEXT)`,
-  (error) => {
-    if (error) {
-      return console.error(error.message);
+  db.exec(
+    `CREATE TABLE IF NOT EXISTS ${tableNames.authors} (
+    ${authorFields.id} INTEGER PRIMARY KEY AUTOINCREMENT,
+    ${authorFields.name} TEXT NOT NULL,
+    ${authorFields.description} TEXT,
+    ${authorFields.image} TEXT)`,
+    (error) => {
+      if (error) {
+        return console.error(error.message);
+      }
     }
-  }
-);
+  );
 
-db.exec(
-  `CREATE TABLE IF NOT EXISTS albums (
+  db.exec(
+    `CREATE TABLE IF NOT EXISTS albums (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT NOT NULL,
     description TEXT,
     image TEXT,
     creationDate TEXT)`,
 
-  (error) => {
-    if (error) {
-      return console.error(error.message);
+    (error) => {
+      if (error) {
+        return console.error(error.message);
+      }
     }
-  }
-);
+  );
+}
 
 function createDbConnection() {
   if (fs.existsSync(filepath)) {
