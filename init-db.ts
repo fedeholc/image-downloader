42f90baebc2cad18c252474367a1262b5cc381bb
Federico Holc <federicoholc@gmail.com>
2024-08-05 14:24:35 -0300

    chore: Refactor database initialization and table creation

--- init-db.ts
+++ init-db.ts
@@ -4,35 +4,111 @@ const filepath = "./images.db";
 
 //const tables = { images: "images", authors: "authors", albums: "albums" };
 
-enum tableNames {
-  images = "images",
-  authors = "authors",
-  albums = "albums",
+enum TableNames {
+  image = "image",
+  author = "author",
+  album = "album",
 }
 
-enum authorFields {
+enum AuthorFields {
   id = "id",
   name = "name",
   description = "description",
   image = "image",
 }
 
-//delete the database file if it exists
-if (fs.existsSync(filepath)) {
-  fs.unlink(filepath, (error) => {
+type Author = {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+};
+
+enum ImageFields {
+  id = "id",
+  url = "url",
+  description = "description",
+  source = "source",
+  albumId = "albumId",
+  authorId = "authorId",
+}
+
+type Image = {
+  id: number;
+  url: string;
+  description: string;
+  source: string;
+  albumId: number;
+  authorId: number;
+};
+
+enum AlbumFields {
+  id = "id",
+  nombre = "nombre",
+  description = "description",
+  image = "image",
+  dateCreated = "dateCreated",
+}
+type Album = {
+  id: number;
+  nombre: string;
+  description: string;
+  image: string;
+  dateCreated: string;
+};
+
+
+//* MAIN *
+
+await deleteDbFile(filepath);
+
+//create a database if it doesn't exist
+const db = createDbConnection();
+
+deleteTable(TableNames.album, db);
+deleteTable(TableNames.author, db);
+deleteTable(TableNames.image, db);
+
+createTables(db);
+
+insertAuthor({ id: 0, name: "Eugene Smith", description: "American photojournalist", image: "smith.jpg" }, db);
+
+closeDbConnection(db);
+
+//* FUNCTIONS *
+
+function closeDbConnection(db: sqlite3.Database) {
+  db.close((error) => {
     if (error) {
       return console.error(error.message);
+    } else {
+      console.log("Database connection closed");
     }
   });
 }
 
+async function deleteDbFile(filepath: string): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(filepath)) {
+      fs.unlink(filepath, (error) => {
+        if (error) {
+          console.error(error.message)
+          reject(false);
+        }
+        else {
+          console.log("Database file deleted");
+          resolve(true);
+        }
+      });
+    }
+  });
+}
 
-function insertRow() {
-  //const [name, color, weight] = process.argv.slice(2);
-  const [name, color, weight] = ["Jaws", "white", "2000"];
+function insertAuthor(author: Author, db: sqlite3.Database) {
+
   db.run(
-    `INSERT INTO ${tableNames.authors} (name, color, weight) VALUES (?, ?, ?)`,
-    [name, color, weight],
+    `INSERT INTO ${TableNames.author} (${AuthorFields.name}, ${AuthorFields.description}, ${AuthorFields.image}) VALUES (?, ?, ?)`,
+    [author.name, author.description, author.image],
     function (error) {
       if (error) {
         console.error(error.message);
@@ -42,69 +118,71 @@ function insertRow() {
   );
 }
 
-//create a database if it doesn't exist
-const db = createDbConnection();
 
-deleteTables(db);
-createTables(db);
-
-db.close((error) => {
-  if (error) {
-    return console.error(error.message);
-  }
-});
-
-function deleteTables(db: sqlite3.Database) {
-  db.exec("DROP TABLE IF EXISTS images");
-  db.exec("DROP TABLE IF EXISTS authors");
-  db.exec("DROP TABLE IF EXISTS albums");
-
+function deleteTable(tableName: string, db: sqlite3.Database) {
+  db.exec(`DROP TABLE IF EXISTS ${tableName}`, (error) => {
+    if (error) {
+      return console.error(error.message);
+    }
+    else {
+      console.log("Table deleted.");
+    }
+  });
 }
+
 
 function createTables(db: sqlite3.Database) {
 
   db.exec(
-    `CREATE TABLE IF NOT EXISTS images (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    url TEXT NOT NULL,
-    description TEXT,
-    source TEXT,
-    albumId INTEGER,
-    authorId INTEGER,
-    FOREIGN KEY(authorId) REFERENCES authors(id),
-    FOREIGN KEY(albumId) REFERENCES albums(id)
+    `CREATE TABLE IF NOT EXISTS ${TableNames.image} (
+    ${ImageFields.id} INTEGER PRIMARY KEY AUTOINCREMENT,
+    ${ImageFields.url} TEXT NOT NULL,
+    ${ImageFields.description} TEXT,
+    ${ImageFields.source} TEXT,
+    ${ImageFields.albumId} INTEGER,
+    ${ImageFields.authorId} INTEGER,
+    FOREIGN KEY(${ImageFields.authorId}) REFERENCES ${TableNames.author}(id),
+    FOREIGN KEY(${ImageFields.albumId}) REFERENCES ${TableNames.album}(id)
   )`,
     (error) => {
       if (error) {
         return console.error(error.message);
+      } else {
+        console.log("Table created.");
       }
     }
   );
 
   db.exec(
-    `CREATE TABLE IF NOT EXISTS ${tableNames.authors} (
-    ${authorFields.id} INTEGER PRIMARY KEY AUTOINCREMENT,
-    ${authorFields.name} TEXT NOT NULL,
-    ${authorFields.description} TEXT,
-    ${authorFields.image} TEXT)`,
+    `CREATE TABLE IF NOT EXISTS ${TableNames.author} (
+    ${AuthorFields.id} INTEGER PRIMARY KEY AUTOINCREMENT,
+    ${AuthorFields.name} TEXT NOT NULL,
+    ${AuthorFields.description} TEXT,
+    ${AuthorFields.image} TEXT)`,
     (error) => {
       if (error) {
         return console.error(error.message);
+      }
+      else {
+        console.log("Table created.");
       }
     }
   );
 
   db.exec(
-    `CREATE TABLE IF NOT EXISTS albums (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    description TEXT,
-    image TEXT,
-    creationDate TEXT)`,
+    `CREATE TABLE IF NOT EXISTS ${TableNames.album} (
+    ${AlbumFields.id} INTEGER PRIMARY KEY AUTOINCREMENT,
+    ${AlbumFields.nombre} TEXT NOT NULL,
+    ${AlbumFields.description} TEXT,
+    ${AlbumFields.image} TEXT,
+    ${AlbumFields.dateCreated} TEXT)`,
 
     (error) => {
       if (error) {
         return console.error(error.message);
+      }
+      else {
+        console.log("Table created.");
       }
     }
   );
@@ -112,14 +190,18 @@ function createTables(db: sqlite3.Database) {
 
 function createDbConnection() {
   if (fs.existsSync(filepath)) {
-    return new sqlite3.Database(filepath);
+    console.log("Database already exists");
   } else {
-    const db = new sqlite3.Database(filepath, (error) => {
-      if (error) {
-        return console.error(error.message);
-      }
-    });
-    console.log("Connection with SQLite has been established");
-    return db;
+    console.log("Creating database");
   }
+
+  return new sqlite3.Database(filepath, (error) => {
+    if (error) {
+      return console.error(error.message);
+    } else {
+      console.log("Connection with SQLite has been established");
+    }
+
+  });
+
 }
